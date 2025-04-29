9421be4833e8f9976e45a071494e65240b4d5600
David Alzate <davids20xx@outlook.com>
2025-04-29 15:36:57 -0500

    feat: adding dispatch list

--- src/app/despacho/listar/despacho.component.ts
+++ src/app/despacho/listar/despacho.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {DespachoService} from '../../service/despacho/despacho.service';
+import {Despacho} from '../../interface/despacho';
+import {CrearDespachoComponent} from '../crear/crear-despacho.component';
 
 @Component({
   selector: 'app-despacho',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './despacho.component.html',
   styleUrl: './despacho.component.scss'
 })
-export class DespachoComponent {
+export class DespachoComponent  implements OnInit {
+  despacho: Despacho[] = [];
 
+  constructor(
+    private readonly despachoService: DespachoService,
+    private readonly modalService: NgbModal
+  ) {}
+
+  ngOnInit(): void {
+    this.cargarDespacho();
+  }
+
+  cargarDespacho(): void {
+    this.despachoService.getDespacho().subscribe({
+      next: (data) => {
+        this.despacho = data;
+      },
+      error: (error) => {
+        console.error('Error al cargar los despachos:', error);
+      }
+    });
+  }
+
+  abrirModalCrearDespacho(): void {
+    const modalRef = this.modalService.open(CrearDespachoComponent);
+    modalRef.result.then(
+      (result) => {
+        if (result === 'Despacho creado') {
+          this.cargarDespacho();
+        }
+      },
+      (reason) => {
+        console.log(reason);
+      }
+    );
+  }
+
+  editarDespacho(documento: number): void {
+    console.log(`Editar despacho con documento: ${documento}`);
+  }
+
+  eliminarDespacho(documento: number): void {
+    if (confirm(`¿Estás seguro de que deseas eliminar el despacho con documento ${documento}?`)) {
+      this.despachoService.deleteDespacho(documento).subscribe({
+        next: (response) => {
+          console.log('Despacho eliminado exitosamente:', response);
+          this.cargarDespacho();
+        },
+        error: (error) => {
+          console.error(`Error al eliminar el despacho con documento ${documento}:`, error);
+          // Aquí puedes mostrar un mensaje de error al usuario
+        }
+      });
+    }
+  }
 }
