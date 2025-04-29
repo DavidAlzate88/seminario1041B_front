a7e19d28dd3ead5da31fce865eb738eb29eca737
David Alzate <davids20xx@outlook.com>
2025-04-29 12:19:43 -0500

    feat: adding ng-Bootstrap and configs

--- environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  url: "ec2-3-148-247-16.us-east-2.compute.amazonaws.com:8085/api/compras",
-}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrlTransportista: 'http://ec2-44-223-34-117.compute-1.amazonaws.com:8080'
+};
