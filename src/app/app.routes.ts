018692e27274645d45a5e160676e2c2b4b3be929
David Alzate <davids20xx@outlook.com>
2025-04-29 20:44:53 -0500

    fIX: remove unused routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ import {DespachoComponent} from './components/despacho/listar/despacho.component
 
 export const routes: Routes = [
   { path: '', component: ListarTransportistaComponent },
+  { path: 'transportista', component: ListarTransportistaComponent },
   { path: 'despacho', component: DespachoComponent },
 ];
